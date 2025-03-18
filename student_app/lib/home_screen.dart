0cb4a83c25ddd7203ab1eeb74938c1ed41a3188f
KshitijH123 <kshitijhapase@gmail.com>
2025-03-18 23:01:09 +0530

    listview builder in home page

--- student_app/lib/home_screen.dart
+++ student_app/lib/home_screen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Map<String, dynamic>? studentData;
+  List<Map<String, dynamic>> studentsData = [];
 
   Future<void> navigateToStudentScreen() async {
     final result = await Navigator.push(
@@ -19,7 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     if (result != null && mounted) {
       setState(() {
-        studentData = result;
+        studentsData.add(result);
       });
     }
   }
@@ -29,51 +29,52 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Student App',
-          style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+          'Student App (${studentsData.length})',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
         ),
         backgroundColor: Colors.lightBlueAccent,
       ),
       body:
-          studentData == null
+          studentsData.isEmpty
               ? Center(
                 child: Text('No Student Data', style: TextStyle(fontSize: 20)),
               )
               : Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder:
-                              (context) => StudentMarksScreen()
-                        ),
-                      );
-                    },
-                    borderRadius: BorderRadius.circular(10),
-                    child: Card(
+                child: ListView.builder(
+                  itemCount: studentsData.length,
+                  itemBuilder: (context, index) {
+                    final student = studentsData[index];
+                    return Card(
                       elevation: 4,
-                      child: Padding(
-                        padding: const EdgeInsets.all(14.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              'Name: ${studentData!['name']}',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
+                      margin: EdgeInsets.symmetric(vertical: 8),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(14.0),
+                        title: Text(
+                          'Name: ${student['name']}',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Total: ${student['marks']['total']}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            setState(() {
+                              studentsData.removeAt(index);
+                            });
+                          },
                         ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 ),
               ),
       floatingActionButton: FloatingActionButton(
