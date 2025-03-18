913bc3d7f38f9384baaef5bba6cc436fd45556e6
KshitijH123 <kshitijhapase@gmail.com>
2025-03-18 14:42:02 +0530

    changes

--- student_app/lib/student_marks_screen.dart
+++ student_app/lib/student_marks_screen.dart
@@ -9,6 +9,11 @@ class StudentMarksScreen extends StatefulWidget {
 
 class _StudentMarksScreenState extends State<StudentMarksScreen> {
   final studentNameController = TextEditingController();
+  final marathiController = TextEditingController();
+  final hindiController = TextEditingController();
+  final englishController = TextEditingController();
+  final scienceController = TextEditingController();
+  final historyController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,21 +25,55 @@ class _StudentMarksScreenState extends State<StudentMarksScreen> {
         ),
         backgroundColor: Colors.lightBlueAccent,
       ),
-
-      body: Padding(
-        padding: EdgeInsets.all(18),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              controller: studentNameController,
-              decoration: InputDecoration(
-                labelText: 'Student Name',
-                border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: studentNameController,
+                decoration: InputDecoration(
+                  labelText: 'Student Name',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 30),
+              buildSubjectRow('• Marathi', marathiController),
+              buildSubjectRow('• Hindi', hindiController),
+              buildSubjectRow('• English', englishController),
+              buildSubjectRow('• Science', scienceController),
+              buildSubjectRow('• History', historyController),
+              SizedBox(height: 30),
+              Center(
+                child: ElevatedButton(
+                  onPressed:(){
+                  
+                }, child: Text('Save',style: TextStyle(fontSize: 24,fontWeight: FontWeight.w400),),),
+              )
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget buildSubjectRow(String subject, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('$subject :', style: TextStyle(fontSize: 24)),
+          SizedBox(
+            width: 100,
+            child: TextFormField(
+              controller: controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+            ),
+          ),
+        ],
       ),
     );
   }
