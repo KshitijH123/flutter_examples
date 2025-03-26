c874312b6578f0071bdbd3e40fe6b07df176409c
KshitijH123 <kshitijhapase@gmail.com>
2025-03-26 20:51:19 +0530

    ui updates

--- student_sqlite/lib/home_screen.dart
+++ student_sqlite/lib/home_screen.dart
@@ -1,47 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:student_sqlite/model/students_model.dart';
+import 'package:student_sqlite/student_marks_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.title});
-
-  final String title;
+  const HomeScreen({super.key, required String title});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
+  List<StudentsModel> students = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> navigateToStudentScreen() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => StudentMarksScreen()),
+    );
+
+    if (result != null && mounted) {
+      setState(() {
+        students.add(result);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text(
+          'Students Score ',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
         ),
+        backgroundColor: Colors.lightBlueAccent,
       ),
+      body:
+          students.isEmpty
+              ? Center(
+                child: Text('No Student Data', style: TextStyle(fontSize: 20)),
+              )
+              : Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ListView.builder(
+                  itemCount: students.length,
+                  itemBuilder: (context, index) {
+                    final student = students[index];
+                    return Card(
+                      elevation: 4,
+                      margin: EdgeInsets.symmetric(vertical: 8),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(14.0),
+                        title: Text(
+                          'Name: ${student.name}',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      subtitle: Text(
+                          'Percentage: ${((student.totalMarks() / 500) * 100).toStringAsFixed(2)}%',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.green,
+                          ),
+                        ),
+                       trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text("Confirm Delete"),
+                                  content: Text(
+                                    "Are you sure you want to delete this item?",
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(
+                                          context,
+                                        ).pop(); 
+                                      },
+                                      child: Text("Cancel"),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          students.removeAt(index);
+                                        });
+                                        Navigator.of(
+                                          context,
+                                        ).pop(); 
+                                      },
+                                      child: Text(
+                                        "Delete",
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
+
+                      ),
+                    );
+                  },
+                ),
+              ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: navigateToStudentScreen,
+        child: Icon(Icons.person_add),
       ),
     );
   }
---
+++ student_sqlite/lib/model/students_model.dart
@@ -0,0 +1,56 @@
+class StudentsModel {
+  final int? id;
+  final String name;
+  final int marathiMarks;
+  final int hindiMarks;
+  final int englishMarks;
+  final int scienceMarks;
+  final int historyMarks;
+
+  StudentsModel({
+    this.id,
+    required this.name,
+    required this.marathiMarks,
+    required this.hindiMarks,
+    required this.englishMarks,
+    required this.scienceMarks,
+    required this.historyMarks,
+  });
+
+  int totalMarks() {
+    return marathiMarks + hindiMarks + englishMarks + scienceMarks + hindiMarks;
+  }
+
+  factory StudentsModel.fromJson(Map<String, dynamic> json) {
+    return StudentsModel(
+      name: json[StudentsModelKey.name],
+      marathiMarks: json[StudentsModelKey.marathiMarks],
+      hindiMarks: json[StudentsModelKey.hindiMarks],
+      englishMarks: json[StudentsModelKey.englishMarks],
+      scienceMarks: json[StudentsModelKey.scienceMarks],
+      historyMarks: json[StudentsModelKey.historyMarks],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      StudentsModelKey.id: id,
+      StudentsModelKey.name: name,
+      StudentsModelKey.marathiMarks: marathiMarks,
+      StudentsModelKey.hindiMarks: hindiMarks,
+      StudentsModelKey.englishMarks: englishMarks,
+      StudentsModelKey.scienceMarks: scienceMarks,
+      StudentsModelKey.historyMarks: historyMarks,
+    };
+  }
+}
+
+class StudentsModelKey {
+  static const id = 'id';
+  static const name = 'name';
+  static const marathiMarks = 'marathiMarks';
+  static const hindiMarks = 'hindiMarks';
+  static const englishMarks = 'englishMarks';
+  static const scienceMarks = 'scienceMarks';
+  static const historyMarks = 'historyMarks';
+}
