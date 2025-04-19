b3dbe94b540d571ca245a3be2c5ec172db4979f2
KshitijH123 <kshitijhapase@gmail.com>
2025-04-19 21:44:18 +0530

    fetch reviews

--- product_catalog/lib/service/api_service.dart
+++ product_catalog/lib/service/api_service.dart
@@ -3,11 +3,11 @@ import 'package:http/http.dart' as http;
 import 'package:product_catalog/model/product_model.dart';
 
 class ApiService {
-  final String apiUrl = "https://dummyjson.com/products";
+  final String baseUrl = "https://dummyjson.com/products";
 
   Future<List<Product>> fetchProducts() async {
     try {
-      final response = await http.get(Uri.parse(apiUrl));
+      final response = await http.get(Uri.parse(baseUrl));
 
       if (response.statusCode == 200) {
         final jsonData = json.decode(response.body);
@@ -18,4 +18,27 @@ class ApiService {
     } catch (e) {}
     return [];
   }
+
+Future<List<Review>> fetchReviews(int productId) async {
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/$productId'));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final List reviewsJson = jsonData['reviews'] ?? [];
+
+        return reviewsJson.map((r) {
+          return Review(
+            reviewerName: r['user'] ?? 'Anonymous',
+            rating: r['rating'] ?? 0,
+            comment: r['comment'] ?? '',
+            date: DateTime.now(),
+          );
+        }).toList();
+      }
+    } catch (e) {
+}
+    return [];
+  }
+
 }
