8b260eb408fc2627ed2447d46228064b50bd21f4
KshitijH123 <kshitijhapase@gmail.com>
2025-04-22 20:42:02 +0530

    home screen

--- product_catalog/lib/service/api_service.dart
+++ product_catalog/lib/service/api_service.dart
@@ -3,19 +3,19 @@ import 'package:http/http.dart' as http;
 import 'package:product_catalog/model/product_model.dart';
 
 class ApiService {
-  final String baseUrl = ' https://dummyjson.com/products';
+  final String baseUrl = 'https://dummyjson.com/products';
+  
+  Future <List<Product>> fetchProduct() async{
+    try{
+      final responce = await http.get (Uri.parse(baseUrl));
 
-  Future<List<Product>> fetchProduct() async {
-    try {
-      final responce = await http.get(Uri.parse(baseUrl));
-
-      if (responce.statusCode == 200) {
+      if (responce.statusCode == 200){
         final jsonData = json.decode(responce.body);
-        final List productsJson = jsonData['products'];
+        final List productsJson = jsonData ['products'];
 
-        return productsJson.map((item) => Product.fromJson(item)).toList();
-      }
-    } catch (e) {}
+        return productsJson.map((item) => Product.fromJson(item)).toList(); 
+      } 
+    }catch (e) {}
     return [];
   }
   Future<List<Review>> fetchReviews(int productId) async {
