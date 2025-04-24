5382001893efba1739f999372bc87c79190a19be
KshitijH123 <kshitijhapase@gmail.com>
2025-04-24 19:59:30 +0530

    practice

---
+++ dart_examples/practice/reverce_string.dart
@@ -0,0 +1,11 @@
+void main() {
+  String input = "hello";
+  print("Reversed: ${reverse(input)}");
+}
+
+
+String reverse(String str) {
+  if (str.isEmpty) return "";
+  return reverse(str.substring(1)) + str[0];
+}
+
