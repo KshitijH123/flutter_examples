38e646feb868a1985e2a3e486466f84262dfda41
KshitijH123 <kshitijhapase@gmail.com>
2025-04-28 15:43:39 +0530

    3 : 43 abstract class

--- dart_examples/list_examples/students/failed_students.dart
+++ dart_examples/list_examples/students/failed_students.dart
@@ -1,5 +1,23 @@
 // Failed Students name
 
+// import 'student_model.dart';
+
+// void main() {
+//   final List<Student> students = [
+//     Student(name: 'kshitij', percentage: 35, isPassed: false),
+//     Student(name: 'rushi', percentage: 67, isPassed: true),
+//     Student(name: 'om', percentage: 30, isPassed: false),
+//     Student(name: 'om', percentage: 30, isPassed: false),
+//     Student(name: 'harsh', percentage: 32, isPassed: false),
+//   ];
+
+//   final failedStudents = students
+//       .where((student) => !student.isPassed)
+//       .toList();
+
+//   final studentName = failedStudents.map((student) => student.name).toList();
+//   print(studentName);
+// }
 
 import 'student_model.dart';
 
@@ -8,14 +26,16 @@ void main() {
     Student(name: 'kshitij', percentage: 35, isPassed: false),
     Student(name: 'rushi', percentage: 67, isPassed: true),
     Student(name: 'om', percentage: 30, isPassed: false),
-    Student(name: 'yash', percentage: 65, isPassed: true),
+    Student(name: 'yash', percentage: 30, isPassed: false),
     Student(name: 'harsh', percentage: 32, isPassed: false),
   ];
 
-  final failedStudents = students
-      .where((student) => !student.isPassed)
-      .toList();
+  List<String> studentName = [];
 
-  final studentName = failedStudents.map((student) => student.name).toList();
+  for (var student in students) {
+    if (!student.isPassed) {
+      studentName.add(student.name);
+    }
+  }
   print(studentName);
 }
