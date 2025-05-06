62b57a149a703353c0f0d8acfc29b489cabd7de7
KshitijH123 <kshitijhapase@gmail.com>
2025-05-06 14:23:06 +0530

    12 hours watch

--- clock_flutter/clock_flutter/lib/home_screen.dart
+++ clock_flutter/clock_flutter/lib/home_screen.dart
@@ -14,7 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text(
           'Clock',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
         elevation: 2,
@@ -26,15 +26,15 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                width: 85,
-                height: 85,
+                width: 95,
+                height: 95,
                 color: Colors.grey[300],
                 child: Center(
                   child: StreamBuilder(
                     stream: Stream.periodic(const Duration(seconds: 1)),
                     builder: (context, snapshot) {
                       return Text(
-                        DateTime.now().hour.toString().padLeft(2, '0'),
+                        DateTime.now().hour% 12 == 0? "12" : (DateTime.now().hour % 12).toString().padLeft(2, '0'),
                         style: const TextStyle(
                           fontSize: 40,
                           fontWeight: FontWeight.bold,
@@ -52,8 +52,8 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                width: 85,
-                height: 85,
+                width: 95,
+                height: 95,
                 color: Colors.grey[300],
                 child: Center(
                   child: StreamBuilder(
@@ -79,8 +79,8 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                width: 85,
-                height: 85,
+                width: 95,
+                height: 95,
                 color: Colors.grey[300],
                 child: Center(
                   child: StreamBuilder(
