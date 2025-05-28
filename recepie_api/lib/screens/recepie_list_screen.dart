f76f35fe0162f6d4a8ff5d72d9872c354b9bf9d7
KshitijH123 <kshitijhapase@gmail.com>
2025-05-28 16:56:15 +0530

    services and model class

---
+++ recepie_api/lib/screens/recepie_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecepieListScreen extends StatefulWidget {
+  const RecepieListScreen({super.key});
+
+  @override
+  State<RecepieListScreen> createState() => _RecepieListScreenState();
+}
+
+class _RecepieListScreenState extends State<RecepieListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
