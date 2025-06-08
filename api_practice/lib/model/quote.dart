5dc98cef74cf40f2956666e6f50814285a180180
KshitijH123 <kshitijhapase@gmail.com>
2025-06-08 20:37:11 +0530

    model class

--- api_practice/lib/model/quote.dart
+++ api_practice/lib/model/quote.dart
@@ -1,14 +1,10 @@
 class Quote {
-  final int id;
   final String quote;
   final String author;
 
-  Quote({required this.id, required this.quote, required this.author});
+  Quote({required this.quote, required this.author});
 
   factory Quote.fromJson(Map<String, dynamic> json) {
-    return Quote(id: json['id'], quote: json['quote'], author: json['author']);
-  }
-  Map<String, dynamic> toJson() {
-    return {'id': id, 'quote': quote, 'author': author};
+    return Quote(quote: json['quote'], author: json['author']);
   }
 }
