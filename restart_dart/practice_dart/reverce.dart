c2b533242444aed35018da5455d138fc958599b3
KshitijH123 <kshitijhapase@gmail.com>
2025-06-15 15:04:07 +0530

    reverce loop

---
+++ restart_dart/practice_dart/reverce.dart
@@ -0,0 +1,9 @@
+void main() {
+  var num = 5;
+  final number = [];
+
+  for (var i = num; i >= 1; i--) {
+    number.add(i);
+  }
+  print(number);
+}
---
+++ restart_dart/practice_dart/reverce_string.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> strings = ['apple', 'banana', 'cherry', 'date'];
+
+  for (int i = strings.length - 1; i >= 0; i--) {
+    String reversedString = strings[i].split('').reversed.join('');
+    print(reversedString);
+  }
+}
