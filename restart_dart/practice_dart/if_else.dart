1f24d15b5bc49109aea8c6f75e76979090058857
KshitijH123 <kshitijhapase@gmail.com>
2025-06-16 14:55:53 +0530

    star and if else

---
+++ restart_dart/practice_dart/if_else.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 25;
+  if (age <= 12) {
+    print('You are a child.');
+  } else if (age <= 19) {
+    print('You are a teenager.');
+  } else if (age <= 34) {
+    print('You are an adult.');
+  } else {
+    print('You are a senior.');
+  }
+}
