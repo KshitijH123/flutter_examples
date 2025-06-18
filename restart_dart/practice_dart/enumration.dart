659c7131b5c9e204533e8c0c789f7af455d67764
KshitijH123 <kshitijhapase@gmail.com>
2025-06-18 18:06:00 +0530

    enumration example

---
+++ restart_dart/practice_dart/enumration.dart
@@ -0,0 +1,21 @@
+enum Color { red, green, blue, yellow }
+
+void main() {
+  Color myColor = Color.blue;
+
+  switch (myColor) {
+    case Color.red:
+      print('The color is Red');
+      break;
+    case Color.green:
+      print('The color is Green');
+      break;
+    case Color.blue:
+      print('The color is Blue');
+      break;
+    case Color.yellow:
+      print('The color is Yellow');
+      break;
+    }
+       
+}
