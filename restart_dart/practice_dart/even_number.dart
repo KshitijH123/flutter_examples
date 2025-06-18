ba38ebf87af9abd09dced63b4004870e532728d9
KshitijH123 <kshitijhapase@gmail.com>
2025-06-18 11:20:49 +0530

    multiplication and even number

---
+++ restart_dart/practice_dart/even_number.dart
@@ -0,0 +1,9 @@
+void main() {
+  num number = 100;
+
+  for (int i = 0; i < number; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+}
---
+++ restart_dart/practice_dart/multiplication_table.dart
@@ -0,0 +1,8 @@
+void main() {
+  int number = 10;
+
+  for (int k = 1; k <= 10; k++) {
+    int result = number * k;
+    print("$number x $k = $result");
+  }
+}
