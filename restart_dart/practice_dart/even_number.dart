5100cbc1da7932356580084e1784e1cfcd3b4f5e
KshitijH123 <kshitijhapase@gmail.com>
2025-06-18 11:23:26 +0530

    changes

--- restart_dart/practice_dart/even_number.dart
+++ restart_dart/practice_dart/even_number.dart
@@ -3,7 +3,7 @@ void main() {
 
   for (int i = 0; i < number; i++) {
     if (i % 2 == 0) {
-      print(i);
+      print("${i}");
     }
   }
 }
--- restart_dart/practice_dart/multiplication_table.dart
+++ restart_dart/practice_dart/multiplication_table.dart
@@ -3,6 +3,6 @@ void main() {
 
   for (int k = 1; k <= 10; k++) {
     int result = number * k;
-    print("$number x $k = $result");
+    print("*$number x $k = $result");
   }
 }
