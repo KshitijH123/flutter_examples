007e5b8e019b80dd2f44d8d4fe304246e3ebd157
KshitijH123 <kshitijhapase@gmail.com>
2025-06-19 21:54:20 +0530

    calling  api

--- get_api_example/lib/home_screen.dart
+++ get_api_example/lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get_api_example/model/model.dart';
+import 'package:get_api_example/service/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +10,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Quote>?> quoteFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    quoteFuture = ApiServices.instance.fetchQuote();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text('Get Api'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Quotes",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        elevation: 2,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<Quote>?>(
+        future: quoteFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No quotes available'));
+          } else {
+            final quotes = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: quotes.length,
+              itemBuilder: (context, index) {
+                final quote = quotes[index];
+
+                return Card(
+                  margin: const EdgeInsets.all(12.0),
+                  shadowColor: Colors.blueGrey,
+                  elevation: 8,
+                  child: ListTile(
+                    title: Text(
+                      quote.quote,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    subtitle: Text('~ ${quote.author}'),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
