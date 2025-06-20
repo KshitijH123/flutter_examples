072067590641fcea5a11acc21ad96982e348b8cb
KshitijH123 <kshitijhapase@gmail.com>
2025-06-20 18:50:43 +0530

    checking number positive or negative

---
+++ restart_dart/practice_dart/checking_number.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = -3;
+
+  if (num > 0) {
+    print('Number is Positive');
+  } else if (num < 0) {
+    print('Number is Negative');
+  } else {
+    print('Number is 0');
+  }
+}
