7ee0d0514593551eff09eda565d390df7e6b8dfb
KshitijH123 <kshitijhapase@gmail.com>
2025-07-09 22:59:17 +0530

    home screen call

--- api_demo_flutter/lib/home_screen.dart
+++ api_demo_flutter/lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:api_demo_flutter/model/comment_model.dart';
+import 'package:api_demo_flutter/service/api_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,12 +9,48 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<CommentModel>> commentFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    commentFuture = ApiService.instance.fetchComment();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Api Demo'),
+        title: const Text('API Demo'),
+        centerTitle: true,
+        elevation: 2,
+      ),
+      body: FutureBuilder<List<CommentModel>>(
+        future: commentFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No comments found.'));
+          } else {
+            final comments = snapshot.data!;
+            return ListView.builder(
+              itemCount: comments.length,
+              itemBuilder: (context, index) {
+                final comment = comments[index];
+                return ListTile(
+                  title: Text(comment.name),
+                  subtitle: Text(comment.body),
+                  trailing: Text(comment.email),
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
