ad0cb416b5aafa7d221fcef41033575b3c397aa8
KshitijH123 <kshitijhapase@gmail.com>
2025-07-25 12:46:10 +0530

    initState

--- api_responce_demo/lib/home_screen.dart
+++ api_responce_demo/lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:api_responce_demo/model/posts_model.dart';
+import 'package:api_responce_demo/service/api_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,8 +10,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<PostModel>> postModel;
+  @override
+  void initState() {
+    super.initState();
+    postModel = ApiService.instance.fetchModel();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Posts'),
+      centerTitle: true,
+      elevation: 2,
+      ),
+    );
   }
 }
