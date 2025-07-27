5b0b6a1917d165102611b3a1601ccd879bfe7c36
KshitijH123 <kshitijhapase@gmail.com>
2025-07-27 13:36:03 +0530

    home screen call

--- comment_api_demo/lib/home_screen.dart
+++ comment_api_demo/lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:comment_api_demo/model/comment_list.dart';
+import 'package:comment_api_demo/service/api_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,8 +10,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Comment>> commentsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    commentsFuture = ApiService.instance.fetchComments();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Comments')),
+      body: FutureBuilder<List<Comment>>(
+        future: commentsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No comments found.'));
+          }
+
+          final comments = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: comments.length,
+            itemBuilder: (context, index) {
+              final comment = comments[index];
+              return ListTile(
+                title: Text(comment.body),
+                subtitle: Text('by ${comment.user.username}'),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
