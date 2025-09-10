037b8ee355f12ce78aa9a15930bcbe3b6ea451fa
KshitijH123 <kshitijhapase@gmail.com>
2025-09-10 12:15:09 +0530

    login screen

---
+++ login_using_api/lib/login_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:login_using_api/home_screen.dart';
+import 'package:login_using_api/register_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void login() {
+    final name = nameController.text.trim();
+    final password = passwordController.text.trim();
+    if (name == 'Kshitij' && password == '1234') {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen(username: name)),
+      );
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Login failed')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[300],
+        title: Text(
+          'Login Screen',
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600, color: Colors.white70),
+        ),
+        elevation: 2,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                 controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'User Name',
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  hintText: 'Password',
+                ),
+              ),
+            ),
+            SizedBox(height: 24),
+            SizedBox(
+              height: 48,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[300],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.zero
+                    )
+                  ),
+                  onPressed: login,
+                  child: Text('Login', style: TextStyle(fontSize: 20,color: Colors.white54)),
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterScreen()),
+                );
+              },
+              child: Text(
+                'Register User',
+                style: TextStyle(fontSize: 16, color: Colors.red),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
