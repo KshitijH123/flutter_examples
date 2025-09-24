f2a540247e59acff53a5a26c80c1ef789ad01307
KshitijH123 <kshitijhapase@gmail.com>
2025-09-24 19:21:20 +0530

    new login project

--- restart_dart/practice_dart/find_perimeter.dart
+++ restart_dart/practice_dart/find_perimeter.dart
@@ -5,7 +5,7 @@ void main() {
 void findPerimeter() {
   int lenght = 4;
   int width = 3;
-
+  
   int perimeter = 2 * (lenght + width);
   print('Thr Perimeter of lenght $lenght and width $width  is = $perimeter');
 }
